d0b68053c59c581f085a9596733b6aac89f3b836
Manu Peralta <manuel.peralta201894@alumnos.info.unlp.edu.ar>
2025-04-09 00:02:14 -0300

    Agrego formulario, clase colores y mensaje de confirmacion

---
+++ colores.js
@@ -0,0 +1,11 @@
+class Colores {
+    static lista = [
+        { nombre: "Predeterminado", hex: "#000000" },
+        { nombre: "Rojo", hex: "#FF0000" },
+        { nombre: "Verde", hex: "#00FF00" },
+        { nombre: "Azul", hex: "#0000FF" },
+        { nombre: "Amarillo", hex: "#FFFF00" },
+        { nombre: "Naranja", hex: "#FFA500" },
+        { nombre: "Rosa", hex: "#FFC0CB" }
+    ];
+}
